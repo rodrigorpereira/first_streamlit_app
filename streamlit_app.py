0a20ba7b3ae4bc76340c692e91dc69e5037a67c2
rodrigorpereira <rodrigorp15@gmail.com>
2023-03-29 18:27:44 -0300

    commit #3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
-streamlit.header('Breajkfast Menu')
-streamlit.text('Omega 3 & Blueberry Meal')
-streamlit.text('Kale, Spinash & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' Breajkfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Meal')
+streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
